bba944e45b4d128f168fa7b2e9cf03204446ba03
ali <anili@my.bcit.ca>
2020-07-16 13:42:47 -0700

    Added shorthand version of a reactive expr

--- plugin.js
+++ plugin.js
@@ -4,16 +4,31 @@ module.exports = function (babel) {
     visitor: {
       Program: {
         enter(innerPath) {
+          function isReactiveFile(comments) {
+            return comments.some(comment =>
+              comment.value.startsWith(' reactive-expr')
+            );
+          }
+
+          if (isReactiveFile(innerPath.parent.comments)) {
+            this.isReactive = true;
+          } else {
+            return;
+          }
+
           this.state = {};
           this.state.reactiveExprs = [];
           this.state.callExprs = [];
           innerPath.traverse(scanner, { state: this.state });
+
           this.state.reactiveExprs = this.state.reactiveExprs.map(expr => {
             expr.statements.reverse();
             return expr;
           });
         },
         exit(innerPath) {
+          console.log(this.state.reactiveExprs);
+          if (!this.isReactive) return;
           innerPath.traverse(insertReactiveExprsVisitor, {
             state: this.state,
             t: t,
@@ -21,7 +36,11 @@ module.exports = function (babel) {
         },
       },
       VariableDeclaration(innerPath) {
-        if (innerPath.node.declarations[0].id.name !== 'ReactiveCall') return;
+        if (
+          innerPath.node.declarations[0].id.name !== 'ReactiveCall' ||
+          !this.isReactive
+        )
+          return;
         this.state.reactiveExprs.forEach(expr => {
           expr.staticState.forEach(s => {
             innerPath.insertAfter(s);
@@ -41,6 +60,8 @@ const insertReactiveExprsVisitor = {
 
     for (let i = 0; i < this.state.reactiveExprs.length; i++) {
       let expr = this.state.reactiveExprs[i];
+      // console.log(expr);
+      // console.log('--------------------------------------------------------');
       if (expr.onCallParams.funcName === funcName) {
         if (argNamesEqual(expr.onCallParams.argNames, argNames)) {
           expr.statementsPath.traverse(replacePlaceholdersWithActual, {
@@ -56,6 +77,8 @@ const insertReactiveExprsVisitor = {
   },
   LabeledStatement(innerPath) {
     if (innerPath.node.label.name === 'ReactiveExpr') {
+      innerPath.remove();
+    } else if (innerPath.node.label.name.startsWith('$')) {
       innerPath.remove();
     }
   },
@@ -75,6 +98,49 @@ const scanner = {
       innerPath.traverse(reactiveExprVisitor, {
         reactiveExprs: this.state.reactiveExprs,
       });
+    } else if (innerPath.node.label.name.startsWith('$')) {
+      innerPath.traverse(shorthandExprVisitor, {
+        reactiveExprs: this.state.reactiveExprs,
+        funcName: innerPath.node.label.name.slice(1),
+      });
+    }
+  },
+};
+
+const shorthandExprVisitor = {
+  BlockStatement: {
+    exit(innerPath) {
+      if (
+        innerPath.parent.type === 'LabeledStatement' &&
+        innerPath.parent.label.name.startsWith('$')
+      ) {
+        this.reactiveExprs.push({
+          onCallParams: this.onCallParams,
+          statements: innerPath.node.body.slice(1),
+          statementsPath: innerPath,
+          staticState: this.hasStaticState ? this.staticState.declarations : [],
+          hasStaticState: this.hasStaticState,
+        });
+      }
+    },
+  },
+  LabeledStatement(innerPath) {
+    if (innerPath.node.label.name === 'Params') {
+      this.onCallParams = {
+        funcName: this.funcName,
+        argNames: innerPath.node.body.expression.elements.map(el => {
+          return getArgName(el);
+        }),
+      };
+    } else if (innerPath.node.label.name === 'SharedState') {
+      this.hasStaticState = true;
+      let staticState = {
+        declarations: [],
+        variables: [],
+      };
+      innerPath.traverse(staticStateVisitor, { staticState });
+      this.staticState = staticState;
+      innerPath.remove();
     }
   },
 };
@@ -104,7 +170,7 @@ const reactiveExprVisitor = {
       };
       innerPath.traverse(onCallVisitor, { onCallParams });
       this.onCallParams = onCallParams;
-    } else if (innerPath.node.label.name === 'StaticState') {
+    } else if (innerPath.node.label.name === 'SharedState') {
       this.hasStaticState = true;
       let staticState = {
         declarations: [],
@@ -166,8 +232,10 @@ function argNamesEqual(exprArgs, actualArgs) {
 function getArgName(argNode) {
   if (argNode.name) {
     return argNode.name;
-  } else {
+  } else if (argNode.object) {
     return argNode.object.name + '[' + argNode.property.value + ']';
+  } else if (argNode.type === 'NumericLiteral') {
+    return argNode.value;
   }
 }
 
